1b18c2a9910c2acdef129f6d73f22d2a775eaea9
William Lindahl <54119288+Adgnascor@users.noreply.github.com>
2020-03-11 13:37:07 +0100

    @Adgnascor Added info to TrailTrack and added TrainTrack properties and constructor

--- Source/TrainTrack/Classes/TrainTrack.cs
+++ Source/TrainTrack/Classes/TrainTrack.cs
@@ -8,5 +8,16 @@ namespace TrainTrack.Classes
         public Enum SwitchPlacement { get; set; }
         public Enum Crossing { get; set; }
         public int Distance { get; set; }
+        public Station StationID { get; set; }
+
+        public TrainTrack(int ID,int StationPlacement,Enum SwitchPlacement,Enum Crossing,int Distance, Station StationID)
+        {
+            this.ID = ID;
+            this.StationPlacement = StationPlacement;
+            this.SwitchPlacement = SwitchPlacement;
+            this.Crossing = Crossing;
+            this.Distance = Distance;
+            this.StationID = StationID;
+        }
     }
 }
