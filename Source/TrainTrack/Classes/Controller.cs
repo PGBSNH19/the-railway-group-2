f490ecc45866f57ec1b2a4dedfe4beda70740964
erifredrika <erifredrika@gmail.com>
2020-03-16 14:09:21 +0100

    Merging
    @erifredrika
    merging versions

--- Source/TrainTrack/Classes/Controller.cs
+++ Source/TrainTrack/Classes/Controller.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 namespace TrainTrack.Classes
 {
-    public class Controller : IController
+    public class Controller //: IController
     {
         public int TrainID { get; set; }
         public int StationID { get; set; }
@@ -12,36 +14,84 @@ namespace TrainTrack.Classes
         public TimeSpan Arrival { get; set; }
 
         TimeSpan addMin = TimeSpan.FromMinutes(01);
+        public TimeTable myTimeTable;
 
+        
 
-        public IController StartTrain(TimeTable time, Train train)
+        public void StartTrain(TimeTable time, Train train)
         {
-            Departure = TimeSpan.Parse(time.DepartureTime);
-
-            var dpTime = TimeSpan.Parse(time.DepartureTime);
-            var avTime = TimeSpan.Parse(time.ArrivalTime);
-            for(int i = 0; i < Departure.Minutes; i++)
-            {
-
-                if ( avTime <= dpTime)
+            var train2TT = myTimeTable.TimeTables;
+            
+                for (int i = 0; i < train2TT.Count - 1; i++)
                 {
+                    TimeSpan addOneMinute = TimeSpan.FromMinutes(1);
+                    DateTime departureTime = DateTime.Parse(train2TT[i].DepartureTime);
+                    DateTime arrivalTime = DateTime.Parse(train2TT[i + 1].ArrivalTime);
 
-                    Console.WriteLine($"chuu choo");
+                    Console.WriteLine($"Departure Time: { departureTime.ToString("HH:mm") }");
+                    Console.WriteLine($"Train: { train.Name }");
+                    Console.WriteLine();
+
+                    if (i == 1)
+                    {
+                        Console.WriteLine("Train is halting for 2 minutes...before continuing the journey!");
+                        Console.WriteLine();
+                        Console.WriteLine($"Departure Time: { departureTime.ToString("HH:mm") }");
+                        Thread.Sleep(1000);
+                    }
+
+                    while (departureTime < arrivalTime)
+                    {
+                        Console.WriteLine($"choo choo { departureTime.ToString("HH:mm") }");
+                        departureTime += addOneMinute;
+                        arrivalTime.AddMinutes(addOneMinute.Minutes);
+                        Thread.Sleep(200);
+                    }
+                    Console.WriteLine($"Arrival Time: { arrivalTime.ToString("HH:mm") }");
+                    Console.WriteLine();
                 }
-                else
-                {
-                    Console.WriteLine($"Train stopped at Saturday {avTime} o´clock and Departed at Friday {dpTime}!");
-                    break;
+            
 
-                }
+            //Departure = TimeSpan.Parse(time.DepartureTime);
 
-                dpTime -= addMin;
-                Thread.Sleep(1000);
-            }
+            //var dpTime = TimeSpan.Parse(time.DepartureTime);
+            //var avTime = TimeSpan.Parse(time.ArrivalTime);
+            //for(int i = 0; i < Departure.Minutes; i++)
+            //{
+
+            //    if ( avTime <= dpTime)
+            //    {
+
+            //        Console.WriteLine($"chuu choo");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Train stopped at Saturday {avTime} o´clock and Departed at Friday {dpTime}!");
+            //        break;
+
+            //    }
+
+            //    dpTime -= addMin;
+            //    Thread.Sleep(1000);
+            //}
 
 
-            return this;
+           
         }
+
+
+        private static Train CheckForTrain(List<Train> trains, int check)
+        {
+            var train = trains.Where(t => t.ID == check).ToList().First();
+            return train;
+        }
+
+        private static List<TimeTable> FollowTimeTable(List<TimeTable> times, Train train)
+        {
+            var timeTable = times.Where(t => t.TrainID == train.ID).ToList();
+            return timeTable;
+        }
+
 
         public IController StopTrain(Train train)
         {
--- Source/TrainTrack/Program.cs
+++ Source/TrainTrack/Program.cs
@@ -7,29 +7,33 @@ namespace TrainTrack
     {
         static void Main(string[] args)
         {
-            var pathTrain = @"/Users/william/OneDrive/Documents/GIT_local/the-railway-group-2/Data/trains.txt";
-            Train train = new Train();
-            foreach (var _train in train.ReadFile(pathTrain))
-            {
-
-            }
-
-            var flyingScottsMan = train.Trains[0];
 
 
-            var pathTime = @"/Users/william/OneDrive/Documents/GIT_local/the-railway-group-2/Data/timetable.txt";
-            TimeTable times = new TimeTable();
-            foreach (var _time in times.ReadFile(pathTime))
-            {
-
-            }
-
-            TimeTable time = times.TimeTables[1];
 
 
-            Console.WriteLine(time.DepartureTime);
+            //var pathTrain = @"Data/trains.txt";
+            //Train train = new Train();
+            //foreach (var _train in train.ReadFile(pathTrain))
+            //{
 
-            _ = new Controller().StartTrain(time, flyingScottsMan);
+            //}
+
+            //var flyingScottsMan = train.Trains[0];
+
+
+            //var pathTime = @"Data/timetable.txt";
+            //TimeTable times = new TimeTable();
+            //foreach (var _time in times.ReadFile(pathTime))
+            //{
+
+            //}
+
+            //TimeTable time = times.TimeTables[1];
+
+
+            //Console.WriteLine(time.DepartureTime);
+
+          
             
 
         }
