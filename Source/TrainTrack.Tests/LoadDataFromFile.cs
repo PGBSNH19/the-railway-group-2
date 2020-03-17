73db0977ff6f1dd2afa5e68080241fe970de3d1f
Murat <m.kugic@gmail.com>
2020-03-17 14:00:37 +0100

    Added test
    @muratkugic: added test

---
+++ Source/TrainTrack.Tests/LoadDataFromFile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TrainTrack.Classes;
+using Xunit;
+
+namespace TrainTrack.Tests
+{
+    public class LoadDataFromFile
+    {
+        [Fact]
+        public void Test1()
+        {
+            Train train = new Train();
+            List<Train> trainsList = train.Trains;
+        }
+    }
+}
